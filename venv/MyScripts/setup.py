e92ba40992175c603228a9f181edbda79135e2b2
kwbln <44318228+kwbln@users.noreply.github.com>
2018-11-18 21:29:16 +0100

    Feierabend

---
+++ venv/MyScripts/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ASE',
+    version='',
+    packages=[''],
+    package_dir={'': 'venv/MyScripts'},
+    url='',
+    license='',
+    author='kwagn',
+    author_email='',
+    description=''
+)
