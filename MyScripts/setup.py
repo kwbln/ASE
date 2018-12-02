196c7f024a4d896bb9292685cd9dd78e20c9a1c3
kwbln <44318228+kwbln@users.noreply.github.com>
2018-12-02 15:24:17 +0100

    restart

--- MyScripts/setup.py
+++ MyScripts/setup.py
@@ -1,13 +1,4 @@
-from setuptools import setup
-
-setup(
-    name='ASE',
-    version='',
-    packages=[''],
-    package_dir={'': 'venv/MyScripts'},
-    url='',
-    license='',
-    author='kwagn',
-    author_email='',
-    description=''
-)
+access_token = '00f8c688c96a41c9d6fe8925df60ff4ca7819dea'
+authorization = 'Bearer fa6bea0d2ebe5948eef6e965424305b30f6aa1c0'
+working_dir = 'C:/Users/kwagn/Github/ASE/'
+athlet_id = '18042219'
