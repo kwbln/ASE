94d8da5732a9e0abc9fe064e60ec2e7fe42211ca
kwbln <44318228+kwbln@users.noreply.github.com>
2019-01-07 11:21:43 +0100

    relative path

--- MyScripts/setup.py
+++ MyScripts/setup.py
@@ -1,4 +1,4 @@
 authorization = 'Bearer fa6bea0d2ebe5948eef6e965424305b30f6aa1c0'
-working_dir = 'C:/Users/kwagn/Github/ASE/'
+working_dir = '../'
 athlet_id = '18042219'
 activity_id = '1988930907'
