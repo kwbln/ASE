f2b8abcf97be1f662f78a5ed1f8a8e2d2f76d8f5
kwbln <44318228+kwbln@users.noreply.github.com>
2019-01-28 08:50:25 +0100

    local build is ok now

---
+++ petproject/DataDemo.py
@@ -0,0 +1,13 @@
+from petproject import DataController as dc
+
+
+class DataDemo(object):
+
+    def __init__(self):
+        pass
+
+    def main(self):
+        dc.DataController().update_view()
+
+
+DataDemo().main()
