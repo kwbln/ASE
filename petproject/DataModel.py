c71ec05a96806a94d844a39a105f3c393685652a
kwbln <44318228+kwbln@users.noreply.github.com>
2019-02-10 06:58:31 +0100

    played around with json

--- petproject/DataModel.py
+++ petproject/DataModel.py
@@ -12,49 +12,43 @@ from petproject import StravaSetup as strs
 class DataModel(object):
 
     def __init__(self):
-        self.auth_url1 = 'https://www.strava.com/api/v3/athlete/activities'
+        self.auth_url1 = 'https://www.strava.com/api/v3/athlete/activities?per_page=1000'
         self.auth_url2 = 'https://www.strava.com/api/v3/activities'
         self.auth_header = {'Authorization': strs.authorization}
 
     def get_activities(self):
-        print("Hello user!")
 
         if os.path.isdir('files') == False:
             os.mkdir('files')
-            print('made data files')
+
         else:
-            print('files dir already availabale')
+            print('ok')
 
         if os.path.isdir('files/data') == False:
             os.mkdir('files/data')
-            print('made data dir')
+
         else:
-            print('data dir already availabale')
+            print('ok')
 
         if os.path.isdir('files/map') == False:
             os.mkdir('files/map')
-            print('made map dir')
         else:
-            print('data map already availabale')
+            print('ok')
 
         if os.path.isfile('./files/data/activities.json') == False:
-            print("generate json files")
 
             json_data = r.get(self.auth_url1, headers=self.auth_header).json()
 
             with codecs.open('./files/data/activities.json', 'w', 'utf8') as f:
                 f.write(json.dumps(json_data, sort_keys=True, ensure_ascii=False))
 
-            print('made json file')
         else:
-            print("json already available")
+            print('ok')
 
         if os.path.isfile('./files/data/activities.xlsx') == False:
-            print("generate xlsx files")
             pd.read_json("./files/data/activities.json").to_excel("./files/data/activities.xlsx")
-            print('made xlsx file')
         else:
-            print("xlsx already available")
+            print('ok')
         # df = pd.read_excel('./files/data/activities.xlsx')
         # df = df.dropna(subset=['start_latitude', 'start_longitude'], how='any')
         # df = df.rename(index=str, columns={'start_latitude': 'lat', 'start_longitude': 'lng'})
